e0ce99e9edab1236b47372b03a947d583a250c06
xinyan <1134043635@qq.com>
2017-12-12 15:13:30 +0800

    添加使用View Cache截图的方法

--- app/src/main/java/com/xinyan/test/MainActivity.java
+++ app/src/main/java/com/xinyan/test/MainActivity.java
@@ -5,26 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ScreenCaptureFragment.OnCaptureFinishedListener {
 
     private ImageView mImageView;
     protected ScreenCaptureFragment mCaptureFragment;
+    protected ImageView mBeautyImage;
+    protected View mDecorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBeautyImage = findViewById(R.id.beauty);
         mImageView = findViewById(R.id.image_view);
+        mDecorView = getWindow().getDecorView();
         mCaptureFragment = new ScreenCaptureFragment();
-        getSupportFragmentManager().beginTransaction().add(mCaptureFragment,"capture").commit();
+        getSupportFragmentManager().beginTransaction().add(mCaptureFragment, "capture").commit();
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.start_capture:
+            case R.id.capture_with_api:
                 mCaptureFragment.startCapture(this);
                 break;
+            case R.id.capture_with_cache:
+                Bitmap bitmap = ViewCaptureUtil.capture(mDecorView);
+                if (bitmap != null) {
+                    mImageView.setImageBitmap(bitmap);
+                }else {
+                    Toast.makeText(this, "获取失败", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
         }
     }
 
---
+++ app/src/main/java/com/xinyan/test/ViewCaptureUtil.java
@@ -0,0 +1,19 @@
+package com.xinyan.test;
+
+import android.graphics.Bitmap;
+import android.view.View;
+
+/**
+ * Created by Xinyan on 2017/12/12.
+ */
+
+public class ViewCaptureUtil {
+
+    public static Bitmap capture(View view){
+        view.setDrawingCacheEnabled(true);
+        Bitmap bitmap = view.getDrawingCache();
+        bitmap = Bitmap.createBitmap(bitmap);
+        view.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+}
